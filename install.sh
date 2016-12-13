84ae2464be55a7811f19584cba49464096497001
Brendan Manning <BrendanManning19380@gmail.com>
2016-12-12 20:57:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,4 @@ git clone https://github.com/brendanmanning/YouTubeNow.git
 cd YouTubeNow/
 npm install --save express@4.10.2
 npm install --save socket.io
-echo "Finished! The server is being started now!" 1>&2
-echo "To start it again run: node server.js (inside this folder)" 1>&2
 node server.js
