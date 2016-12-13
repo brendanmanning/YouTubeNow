890f1949e1acb7ad541431788c335f724a144e86
c0din443 <BrendanManning19380@gmail.com>
2016-12-12 20:54:09 -0500

    Made the script look better

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@ if [ "$(id -u)" != "0" ]; then
    echo "Please run as root or sudo" 1>&2
    exit 1
 fi
-git clone https://github.com/brendanmanning/YouTubeNow.git
+echo "Step 1 of 3"
+git clone https://github.com/brendanmanning/YouTubeNow.git > /dev/null
+echo "Step 2 of 3"
 cd YouTubeNow/
-npm install --save express@4.10.2
-npm install --save socket.io
+npm install --save express@4.10.2 > /dev/null
+echo "Step 3 of 3"
+npm install --save socket.io > /dev/null
+echo "Finished! The server is being started now!"
+echo "To start it again run: node server.js (inside this folder)"
 node server.js
