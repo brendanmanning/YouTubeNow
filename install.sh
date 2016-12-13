d7551400ddb4aa695bf91cb3a36b1e07500a76ed
Brendan Manning <BrendanManning19380@gmail.com>
2016-12-12 20:56:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,10 @@ if [ "$(id -u)" != "0" ]; then
    echo "Please run as root or sudo" 1>&2
    exit 1
 fi
-echo "Step 1 of 3"
-git clone https://github.com/brendanmanning/YouTubeNow.git > /dev/null
-echo "Step 2 of 3"
+git clone https://github.com/brendanmanning/YouTubeNow.git
 cd YouTubeNow/
-npm install --save express@4.10.2 > /dev/null
-echo "Step 3 of 3"
-npm install --save socket.io > /dev/null
-echo "Finished! The server is being started now!"
-echo "To start it again run: node server.js (inside this folder)"
+npm install --save express@4.10.2
+npm install --save socket.io
+echo "Finished! The server is being started now!" 1>&2
+echo "To start it again run: node server.js (inside this folder)" 1>&2
 node server.js
